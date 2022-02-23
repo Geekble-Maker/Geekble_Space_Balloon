04b85c6d31527b9764872ec92f377f2839dfb77c
GeeKIKU <81900509+maker-kiku@users.noreply.github.com>
2022-02-24 01:54:39 +0900

    Update Geekble_Space_Balloon.ino

--- Geekble_Space_Balloon/Geekble_Space_Balloon.ino
+++ Geekble_Space_Balloon/Geekble_Space_Balloon.ino
@@ -6,13 +6,22 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME680 bme; // I2C
 
+#include <SD.h>
+File myFile;
+
+#include <Arduino.h>
+#include <TM1637Display.h>
+#define CLK 2
+#define DIO 3
+TM1637Display display(CLK, DIO);
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("start test");
 
+  // BME680 고도센서 작동 상태 확인 ==================================
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
+    while(1);
   }
 
   // Set up oversampling and filter initialization
@@ -21,33 +30,58 @@ void setup() {
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+
+  // SD카드 모듈 상태 확인
+  Serial.print("Initializing SD card..."); 
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while(1);
+  }
+  Serial.println("initialization done.");
+
+  // FND모듈 초기 설정
+  display.setBrightness(16);//밝기 설정 범위는 0 ~ 16
 }
 
 void loop() {
-  if (! bme.performReading()) {
+  if (! bme.performReading()) { // 고도센서가 작동하지 않을 경우
     Serial.println("Failed to perform reading :(");
     return;
   }
-  Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
-  Serial.println(" *C");
+  
+  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
 
-  Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(" hPa");
+  myFile = SD.open("sensor.txt", FILE_WRITE);
+  if (myFile) { // SD카드가 삽입되어 있다면
+    Serial.print(bme.temperature);
+    Serial.print(" *C  ");
+    myFile.print(bme.temperature);
+    myFile.print(" ");
+  
+    Serial.print(bme.pressure / 100.0);
+    Serial.print(" hPa  ");
+    myFile.print(bme.pressure / 100.0);
+    myFile.print(" ");
+  
+    Serial.print(bme.humidity);
+    Serial.print(" %  ");
+    myFile.print(bme.humidity);
+    myFile.print(" ");
+  
+    Serial.print(bme.gas_resistance / 1000.0);
+    Serial.print(" KOhms  ");
+    myFile.print(bme.gas_resistance / 1000.0);
+    myFile.print(" ");
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
-  Serial.println(" %");
-
-  Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-
-  Serial.println();
+    float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA) + 150;
+    Serial.print(altitude);
+    Serial.println(" m");
+    myFile.println(altitude);
+    display.showNumberDec(int(altitude), false, 4, 0);
+    
+    myFile.close();
+    //myFile.close();
+    //Serial.println("done.");
+  }
   delay(1000);
 }
